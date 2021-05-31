7eee287f5d2adf6d32b5caa7879560addd647994
Jakub Działowy <dzialowy04@gmail.com>
2021-05-31 15:45:16 +0200

    Synchronization mechanisms ex. 4.1.2.3

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -5,6 +5,7 @@
 #include "led.h"
 #include "uart.h"
 #include "string.h"
+#include "keyboard.h"
 
 void LettersTx (void *pvParameters){
 	while(1){
@@ -14,9 +15,19 @@ void LettersTx (void *pvParameters){
 	}
 }
 
+void KeyboardTx (void *pvParameters){
+	while(1){
+		if(eKeyboardRead() != RELASED){
+			Transmiter_SendString("-Keyboard-\n");
+			while(Transmiter_GetStatus()!=FREE){};
+		}
+	}
+}
+
 int main( void ){
 	UART_InitWithInt(300);
 	xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL);
+	xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL);
 	vTaskStartScheduler();
 	while(1);
 }
