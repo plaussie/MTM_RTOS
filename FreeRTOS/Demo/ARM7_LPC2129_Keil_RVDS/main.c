eac2546ec9868a43fdea09239983d73197ac2a24
Jakub Działowy <dzialowy04@gmail.com>
2021-05-31 15:57:19 +0200

    Synchronization mechanisms ex. 4.1.2.4

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -7,10 +7,14 @@
 #include "string.h"
 #include "keyboard.h"
 
+xSemaphoreHandle xSemaphore;
+
 void LettersTx (void *pvParameters){
 	while(1){
+		xSemaphoreTake(xSemaphore,portMAX_DELAY);
 		Transmiter_SendString("-ABCDEEFGH-\n");
 		while(Transmiter_GetStatus()!=FREE){};
+		xSemaphoreGive(xSemaphore);
 		vTaskDelay(300);
 	}
 }
@@ -18,14 +22,18 @@ void LettersTx (void *pvParameters){
 void KeyboardTx (void *pvParameters){
 	while(1){
 		if(eKeyboardRead() != RELASED){
+			xSemaphoreTake(xSemaphore,portMAX_DELAY);
 			Transmiter_SendString("-Keyboard-\n");
 			while(Transmiter_GetStatus()!=FREE){};
+			xSemaphoreGive(xSemaphore);
 		}
 	}
 }
 
 int main( void ){
+	KeyboardInit();
 	UART_InitWithInt(300);
+	vSemaphoreCreateBinary(xSemaphore);
 	xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL);
 	xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL);
 	vTaskStartScheduler();
